6b1b00e52c42b32905fb2a1e1d7b19561af4e466
Prasan Kumar <prasan@moonraft.com>
2020-07-23 16:57:55 +0530

    Card for Databalance

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import './cutom_navbar.dart';
 import './widgets/databalance_card.dart';
-import './themes/colors.dart';
 import './widgets/phone_number.dart';
+import './cutom_bottomnavbar.dart';
+import './widgets/latest_promo.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -16,8 +17,6 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  int _currentIndex = 0;
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,106 +27,13 @@ class _HomeState extends State<Home> {
           children: <Widget>[
             PhoneNumber(),
             DataBalanceCard(),
+            LatestPromoSection(),
           ],
         ),
       ),
-      bottomNavigationBar: ClipRRect(
-        borderRadius: BorderRadius.only(
-          topLeft: Radius.circular(40.0),
-          topRight: Radius.circular(40.0),
-          bottomLeft: Radius.circular(100.0),
-          bottomRight: Radius.circular(100.0),
-        ),
-        child: BottomNavigationBar(
-          showUnselectedLabels: true,
-          onTap: (selectedIndex) {
-            setState(() {
-              _currentIndex = selectedIndex;
-            });
-          },
-          currentIndex: _currentIndex,
-          elevation: 10.0,
-          items: [
-            BottomNavigationBarItem(
-              backgroundColor: AppColors.COLOR_WHITE,
-              icon: new Image.asset(
-                'assets/images/mobile_icon.png',
-                width: 28.0,
-                height: 28.0,
-              ),
-              title: Text(
-                'Mobile',
-                style: TextStyle(
-                    fontSize: 12.0,
-                    color: Color(0xff272727),
-                    letterSpacing: 1.0,
-                    fontFamily: 'AvenirNext',
-                    fontWeight: FontWeight.w700,
-                    backgroundColor:
-                        _currentIndex == 0 ? Color(0xfff1f4ff) : Colors.white),
-              ),
-            ),
-            BottomNavigationBarItem(
-              backgroundColor: AppColors.COLOR_WHITE,
-              icon: new Image.asset(
-                'assets/images/lifestyle_icon.png',
-                width: 28.0,
-                height: 28.0,
-              ),
-              title: Text(
-                'Lifestyle',
-                style: TextStyle(
-                  fontSize: 12.0,
-                  color: Color(0xff272727),
-                  fontFamily: 'AvenirNext',
-                  fontWeight: FontWeight.w700,
-                  letterSpacing: 1.0,
-                  backgroundColor:
-                      _currentIndex == 1 ? Color(0xfff1f4ff) : Colors.white,
-                ),
-              ),
-            ),
-            BottomNavigationBarItem(
-              backgroundColor: Colors.white,
-              icon: new Image.asset(
-                'assets/images/wallet_icon.png',
-                width: 28.0,
-                height: 28.0,
-              ),
-              title: Text(
-                'Wallet',
-                style: TextStyle(
-                  fontSize: 12.0,
-                  color: Color(0xff272727),
-                  fontFamily: 'AvenirNext',
-                  fontWeight: FontWeight.w700,
-                  letterSpacing: 1.0,
-                  backgroundColor:
-                      _currentIndex == 2 ? Color(0xfff1f4ff) : Colors.white,
-                ),
-              ),
-            ),
-            BottomNavigationBarItem(
-              backgroundColor: Colors.white,
-              icon: new Image.asset(
-                'assets/images/more_icon.png',
-                width: 28.0,
-                height: 28.0,
-              ),
-              title: Text(
-                'More',
-                style: TextStyle(
-                    fontSize: 12.0,
-                    color: Color(0xff272727),
-                    fontFamily: 'AvenirNext',
-                    fontWeight: FontWeight.w700,
-                    letterSpacing: 1.0,
-                    backgroundColor:
-                        _currentIndex == 3 ? Color(0xfff1f4ff) : Colors.white),
-              ),
-            ),
-          ],
-        ),
+      bottomNavigationBar: Container(
+        height: 80.0,        
+        child: BottomNavBar(),
       ),
     );
   }
---
+++ lib/widgets/latest_promo.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class LatestPromoSection extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
